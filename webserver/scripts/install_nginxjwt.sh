a74e165515c1ede4a4b09517258826cc3d4f4a83
Jim Kennedy <jim8786453@gmail.com>
2018-02-16 21:21:10 +0000

    NginxJwt (Lua)
    - Fix package install

--- webserver/scripts/install_nginxjwt.sh
+++ webserver/scripts/install_nginxjwt.sh
@@ -5,4 +5,4 @@ wget -c https://github.com/platinummonkey/nginx-jwt/archive/v1.1.0.tar.gz
 tar xvf v1.1.0.tar.gz
 ./nginx-jwt-1.1.0/scripts/build_deps.sh
 mv nginx-jwt-1.1.0/nginx-jwt.lua ~/deploy/bin/nginx-jwt.lua
-mv lib ~/deploy/bin/
+mv lib/* ~/deploy/bin/
